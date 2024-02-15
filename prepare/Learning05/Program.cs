a21f13721a01391d56c36061dc93679a2b2824b5
SpaceSettler <j.devon.richardson@gmail.com>
2024-02-15 00:39:56 -0700

    Wk 5 Assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square square1 = new Square("blue", 23);
+        shapes.Add(square1);
+        Rectangle rect1 = new Rectangle(23, 32, "blue");
+        shapes.Add(rect1);
+        Circle circ1 = new Circle(3.2, "blue");
+        shapes.Add(circ1);
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The {shape.GetColor()} shape has an area of {shape.GetArea()}.");
+        }
     }
 }
